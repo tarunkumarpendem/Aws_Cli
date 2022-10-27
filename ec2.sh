71f68fd5c1aaeab68fce44055a3eb8b79ee16ada
Ubuntu <ubuntu@ip-172-31-94-113.ec2.internal>
2022-10-27 02:30:41 +0000

    added internet gateay script

--- ec2.sh
+++ ec2.sh
@@ -1,20 +1,4 @@
-# create subnet1
-aws ec2 create-subnet \
-    --vpc-id "vpc-0f207b88b749c6d1d" \
-    --cidr-block "10.10.0.0/24" \
-    --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=Subnet1}]" \
+aws ec2 create-internet-gateway \
     --region "us-east-1" \
-    --availability-zone "us-east-1a" \
-    --query "Subnet.SubnetId"
-         
-
-# 
-
-# create subnet2
-aws ec2 create-subnet \
-    --vpc-id "vpc-0f207b88b749c6d1d" \
-    --cidr-block "10.10.1.0/24" \
-    --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=Subnet2}]" \
-    --region "us-east-1" \
-    --availability-zone "us-east-1b" \
-    --query "Subnet.SubnetId"
+    --tag-specifications "ResourceType=internet-gateway,Tags=[{Key=Name,Value=Jenkins-igw}]" \
+    --query "InternetGateway.InternetGatewayId"
