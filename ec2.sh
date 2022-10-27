94e7ad47c969a3285f09eda1c7a45bec6bca4af8
Ubuntu <ubuntu@ip-172-31-94-113.ec2.internal>
2022-10-27 02:34:51 +0000

    added routetables script

--- ec2.sh
+++ ec2.sh
@@ -1,3 +1,13 @@
-aws ec2 attach-internet-gateway \
+# create public route table
+aws ec2 create-route-table \
     --vpc-id "vpc-0f207b88b749c6d1d" \
-    --internet-gateway-id "igw-09379178181ddbd46"
+    --region "us-east-1" \
+    --query "RouteTable.RouteTableId" \
+    --tag-specifications "ResourceType=route-table,Tags=[{Key=Name,Value=Jenkins-Pub-rt}]" 
+
+# create private route table
+aws ec2 create-route-table \
+    --vpc-id "vpc-0f207b88b749c6d1d" \
+    --region "us-east-1" \
+    --query "RouteTable.RouteTableId" \
+    --tag-specifications "ResourceType=route-table,Tags=[{Key=Name,Value=Jenkins-Pvt-rt}]"
