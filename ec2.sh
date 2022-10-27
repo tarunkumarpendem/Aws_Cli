ee09ce50af195509c0aa0c45e77d7cf503ea57b8
Ubuntu <ubuntu@ip-172-31-94-113.ec2.internal>
2022-10-27 02:28:11 +0000

    added subnets script

--- ec2.sh
+++ ec2.sh
@@ -1,5 +1,20 @@
-aws ec2 create-vpc \
-    --cidr-block "10.10.0.0/16" \
-    --tag-specification "ResourceType=vpc,Tags=[{Key=Name,Value=Jenkins_vpc}]" \
+# create subnet1
+aws ec2 create-subnet \
+    --vpc-id "vpc-0f207b88b749c6d1d" \
+    --cidr-block "10.10.0.0/24" \
+    --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=Subnet1}]" \
     --region "us-east-1" \
-    --query "Vpc.VpcId"
+    --availability-zone "us-east-1a" \
+    --query "Subnet.SubnetId"
+         
+
+# 
+
+# create subnet2
+aws ec2 create-subnet \
+    --vpc-id "vpc-0f207b88b749c6d1d" \
+    --cidr-block "10.10.1.0/24" \
+    --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=Subnet2}]" \
+    --region "us-east-1" \
+    --availability-zone "us-east-1b" \
+    --query "Subnet.SubnetId"
