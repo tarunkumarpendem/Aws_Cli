6cb8a9ff95315c67d9746b9ec382da1867958091
Ubuntu <ubuntu@ip-172-31-94-113.ec2.internal>
2022-10-27 02:32:53 +0000

    added internet gateway script

--- ec2.sh
+++ ec2.sh
@@ -1,4 +1,3 @@
-aws ec2 create-internet-gateway \
-    --region "us-east-1" \
-    --tag-specifications "ResourceType=internet-gateway,Tags=[{Key=Name,Value=Jenkins-igw}]" \
-    --query "InternetGateway.InternetGatewayId"
+aws ec2 attach-internet-gateway \
+    --vpc-id "vpc-0f207b88b749c6d1d" \
+    --internet-gateway-id "igw-09379178181ddbd46"
