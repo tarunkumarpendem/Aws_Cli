fe53c16590912b0603f01880dd7e09fdb3735b73
Ubuntu <ubuntu@ip-172-31-94-113.ec2.internal>
2022-10-27 02:54:08 +0000

    added security group script

--- ec2.sh
+++ ec2.sh
@@ -1,14 +1,8 @@
-# Attach subnet1 to public route table
-aws ec2 associate-route-table \
-    --route-table-id "rtb-0921acd3a9469b955" \
-    --subnet-id "subnet-05f3deda72e32439c" \
-    --query "AssociationId" \
-    --region "us-east-1"  
-
-
-# Attach subnet2 to private route table
-aws ec2 associate-route-table \
-    --route-table-id "rtb-033e8d375e1c316b0" \
-    --subnet-id "subnet-03aacccb0a19f10e6" \
-    --query "AssociationId" \
-    --region "us-east-1"
+# create security group 
+aws ec2 create-security-group \
+    --group-name "Jenkins-sg" \
+    --vpc-id "vpc-0f207b88b749c6d1d" \
+    --tag-specifications "ResourceType=security-group,Tags=[{Key=Name,Value=Jenkins_Security_Group}]" \
+    --description "For Jenkins" \
+    --region "us-east-1" \
+    --query "GroupId"
