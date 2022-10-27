98a5efb148a3405948020a425fdd7236e6c37ada
Ubuntu <ubuntu@ip-172-31-94-113.ec2.internal>
2022-10-27 03:24:13 +0000

    added stop ec2 script

--- ec2.sh
+++ ec2.sh
@@ -1,13 +1,4 @@
-# create ec2 instance
-aws ec2 run-instances \
-    --image-id "ami-0149b2da6ceec4bb0" \
-    --instance-type "t2.micro" \
-    --key-name "standard" \
-    --security-group-ids "sg-051700ec3f8a3d7f9" \
-    --subnet-id "subnet-05f3deda72e32439c" \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=Jenkins-Cli-Instance}]" \
-    --associate-public-ip-address \
-    --region "us-east-1" \
-    --query "Instances[0].InstanceId" \
-    --count "1" \
-    --user-data "sudo apt update, sudo apt install apache2 -y"
+# stop ec2 instance
+aws ec2 stop-instances \
+    --instance-ids "i-071a5d5e2eda6e553" \
+    --region "us-east-1"
