bef4b462c128ae5194e2f26a1bb81f84263eb038
Ubuntu <ubuntu@ip-172-31-94-113.ec2.internal>
2022-10-27 02:40:13 +0000

    added route to public route tables script

--- ec2.sh
+++ ec2.sh
@@ -1,13 +1,5 @@
-# create public route table
-aws ec2 create-route-table \
-    --vpc-id "vpc-0f207b88b749c6d1d" \
+aws ec2 create-route \
+    --route-table-id "rtb-0921acd3a9469b955" \
+    --destination-cidr-block "0.0.0.0/0" \
     --region "us-east-1" \
-    --query "RouteTable.RouteTableId" \
-    --tag-specifications "ResourceType=route-table,Tags=[{Key=Name,Value=Jenkins-Pub-rt}]" 
-
-# create private route table
-aws ec2 create-route-table \
-    --vpc-id "vpc-0f207b88b749c6d1d" \
-    --region "us-east-1" \
-    --query "RouteTable.RouteTableId" \
-    --tag-specifications "ResourceType=route-table,Tags=[{Key=Name,Value=Jenkins-Pvt-rt}]"
+    --gateway-id "igw-09379178181ddbd46"
