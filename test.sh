cfe67b829eb8ba4c7aaff6d5c63b68764dc29b07
Tarun <tarunkumarp369@gmail.com>
2022-10-27 21:09:22 +0530

    added changes

--- test.sh
+++ test.sh
@@ -10,6 +10,7 @@ REGION="us-west-2"
 VPC_CIDR="10.10.0.0/16"
 SUBNET_CIDR="10.10.0.0/24"
 SUBNET_AZ="us-west-2a"
+DESTINATION_CIDR="0.0.0.0/0"
 #==============================================================================
 #   DO NOT MODIFY CODE BELOW
 #==============================================================================
@@ -51,6 +52,41 @@ aws ec2 attach-internet-gateway \
     --region $REGION
 echo Vpc_Id = "$Vpc_Id"
 echo Internet-GatewayId = "$Internet_Gateway"
+
+
+
+# create Routetable
+RouteTable_Id=$(aws ec2 create-route-table \
+    --vpc-id $Vpc_Id \
+    --region $REGION \
+    --query '"RouteTable.RouteTableId"' \
+    --tag-specifications "ResourceType=route-table,Tags=[{Key=Name,Value=Jenkins-Rt}]" \
+    --output text) 
+echo $RouteTable-Id = "$RouteTable_Id"    
+
+
+# create route to Routetable
+aws ec2 create-route \
+    --route-table-id $RouteTable_Id \
+    --destination-cidr-block $DESTINATION_CIDR \
+    --region $REGION \
+    --output text \
+    --gateway-id $Internet_Gateway
+echo RouteTable-Id = "$RouteTable_Id" 
+
+
+# Attach subnet to Routetable
+RouteTable_Association_Id=$(aws ec2 associate-route-table \
+    --route-table-id $RouteTable_Id \
+    --subnet-id $Subnet_Id \
+    --query 'AssociationId' \
+    --output text \
+    --region $REGION)
+echo RouteTable-Id = "$RouteTable_Id"
+echo RouteTable-Association-Id = "$RouteTable_Association_Id"   
+
+
+
 
 
 
