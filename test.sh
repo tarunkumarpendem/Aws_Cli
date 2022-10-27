25241c04c374132013702a1f272f6a912e21ad33
Tarun <tarunkumarp369@gmail.com>
2022-10-27 20:37:41 +0530

    added changes

--- test.sh
+++ test.sh
@@ -16,11 +16,11 @@ SUBNET_AZ="us-west-2a"
 # Create VPC
 Vpc_Id=$(aws ec2 create-vpc \
   --cidr-block $VPC_CIDR \
-  --query 'Vpc.{VpcId:VpcId}' \
+  --query 'Vpc.VpcId' \
   --tag-specification "ResourceType=vpc,Tags=[{Key=Name,Value=Jenkins_vpc}]" \
   --output text \
   --region $REGION)
-echo Vpc_Id = $VPC_ID  
+echo Vpc_Id = $Vpc_Id  
 
 
 # Create Public Subnet
@@ -28,8 +28,8 @@ Subnet_Id=$(aws ec2 create-subnet \
   --vpc-id $Vpc_Id \
   --cidr-block $SUBNET_CIDR \
   --availability-zone $SUBNET_AZ \
-  --query 'Subnet.{SubnetId:SubnetId}' \
+  --query 'Subnet.SubnetId' \
   --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=Public_Subnet}]" \
   --output text \
   --region $REGION)
-echo Subnet_Id = $SUBNET_ID
+echo Subnet_Id = $Subnet_Id
